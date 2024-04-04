281187b46f38d048772cd288d7e9d0f6af0c7dc7
AshHehe <109786986+AshHehe@users.noreply.github.com>
2024-04-03 22:32:15 -0400

    Update download.js

--- api/download.js
+++ api/download.js
@@ -1,49 +1,19 @@
-const fetch = require('node-fetch');
-const { send, json } = require('micro');
-
-async function getFileURL(url) {
-    try {
-        const res = await fetch(url);
-        const html = await res.text();
-        const fileURL = html.split('"contentUrl":"')[1]?.split('","')[0];
-        if (fileURL) return fileURL;
-        else return null;
-    } catch (error) {
-        console.error('Error fetching file URL:', error);
-        throw new Error('Failed to fetch file URL');
-    }
+async function uploadMedal() {
+  const medalUrl = document.getElementById('medalUrl').value;
+  const response = await fetch('/api/medalwatermark', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ url: medalUrl })
+  });
+  const data = await response.json();
+  if (data.valid && data.src) {
+    const videoSrc = data.src;
+    const videoElement = document.getElementById('medalVideo');
+    videoElement.src = videoSrc;
+    videoElement.style.display = 'block'; // Show video
+  } else {
+    alert('Invalid Medal video URL!');
+  }
 }
-
-module.exports = async (req, res) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-
-    if (req.method === 'OPTIONS') {
-        return send(res, 200);
-    }
-
-    try {
-        const { url } = await json(req);
-
-        let validUrl = url;
-        if (!validUrl) return send(res, 400, 'Invalid URL');
-        if (!validUrl.includes('medal')) {
-            if (!validUrl.includes('/')) validUrl = 'https://medal.tv/clips/' + validUrl;
-            else return send(res, 400, 'Invalid URL');
-        }
-
-        if (!validUrl.toLowerCase().includes('?mobilebypass=true')) {
-            validUrl += '?mobilebypass=true';
-        }
-
-        validUrl = validUrl.replace('?theater=true', '');
-
-        const src = await getFileURL(validUrl);
-        if (src) return send(res, 200, src);
-        else return send(res, 404, 'Video URL not found');
-    } catch (error) {
-        console.error('Error processing request:', error);
-        return send(res, 500, 'Internal server error');
-    }
-};
