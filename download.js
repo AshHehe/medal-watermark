37ed6a1d3898a2a2c08f24e3e6c5de1ff3865f91
AshHehe <109786986+AshHehe@users.noreply.github.com>
2024-04-03 22:47:01 -0400

    Create download.js

---
+++ download.js
@@ -0,0 +1,19 @@
+async function uploadMedal() {
+  const medalUrl = document.getElementById('medalUrl').value;
+  const response = await fetch('https://medal-watermark.vercel.app', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ url: medalUrl })
+  });
+  const data = await response.json();
+  if (data.valid && data.src) {
+    const videoSrc = data.src;
+    const videoElement = document.getElementById('medalVideo');
+    videoElement.src = videoSrc;
+    videoElement.style.display = 'block'; // Show video
+  } else {
+    alert('Invalid Medal video URL!');
+  }
+}
